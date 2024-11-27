09a41d8d7acadf26261812e6617c508e17e80519
NazmulHasan68 <aishadowzone@gmail.com>
2024-11-27 10:14:12 +0600

    setup react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
 import React from "react"
-import Signup from "./components/Signup"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Signup from '../src/components/Signup'
+import HomePage from "./components/HomePage"
+import Login from "./components/Login"
+
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<HomePage/>
+  },
+  {
+    path:'/register',
+    element:<Signup/>
+  },
+  {
+    path : '/login',
+    element : <Login/>
+  }
+])
 
 export default function App() {
   return (
-    <Signup/>
+    <div className="App">
+      <RouterProvider router={router}/>
+    </div>
   )
 }
