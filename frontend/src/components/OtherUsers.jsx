b70bc01a698ce4fdd5f7e200f2e560825e73eb70
NazmulHasan68 <aishadowzone@gmail.com>
2024-11-27 21:57:51 +0600

    getuser custoom hooks

--- frontend/src/components/OtherUsers.jsx
+++ frontend/src/components/OtherUsers.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import OtherUser from './OtherUser'
+import useGetOtherUsers from '../hooks/useGetOtherUsers'
+import { useSelector } from 'react-redux'
 
 export default function OtherUsers() {
+  useGetOtherUsers()
+  const {otherUsers} = useSelector(store =>store.user)
+  console.log(otherUsers); 
   return (<div className=' scrollbar-hide overflow-auto h-[75%] '>
     <OtherUser/>
     <OtherUser/>
---
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect } from 'react'
+import axios from 'axios'
+import {useDispatch} from 'react-redux'
+import { setOtherUsers } from '../redux/userSlice'
+
+export default function useGetOtherUsers() {
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        const fetchOtherUsers = async()=>{
+            try {
+                const res = await axios.get('http://localhost:8000/api/v1/user',{withCredentials:true})
+                dispatch(setOtherUsers(res.data.otherUser))
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchOtherUsers()
+    },[])
+    
+}
