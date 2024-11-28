2748c3cb2a073f9d7c6d4d8dbf47da312d1b0e44
NazmulHasan68 <aishadowzone@gmail.com>
2024-11-28 23:20:21 +0600

    Create search functionality

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosSearch } from "react-icons/io";
 import OtherUsers from "./OtherUsers";
 import axios from "axios";
 import {toast} from 'react-hot-toast'
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setOtherUsers } from "../redux/userSlice";
 
 export default function Sidebar() {
   const navigate = useNavigate()
+  const [search ,setsearch] = useState('')
+  const {otherUsers} = useSelector(store=>store.user)
+  const dispatch = useDispatch()
+
   const loguthandler =async()=>{
     try {
       const res = await axios.get(`http://localhost:8000/api/v1/user/logout`)
@@ -17,15 +23,29 @@ export default function Sidebar() {
       console.log(error); 
     }
   }
+
+  const searchSubmithandler = (e) =>{
+    e.preventDefault()
+    const conversationUser = otherUsers?.find((user)=> user.fullname.toLowerCase().includes(search.toLowerCase()))
+    if(conversationUser){
+      dispatch(setOtherUsers([conversationUser]))
+    }else{
+      toast.error("User not found")
+    }
+    setsearch('')
+  }
+
   return (
     <div className="p-2 flex flex-col">
-      <form action="" className="flex items-center gap-2">
+      <form action="" onSubmit={searchSubmithandler} className="flex items-center gap-2">
         <input
           type=" text"
+          value={search}
+          onChange={(e)=>setsearch(e.target.value)}
           className="input input-bordered rounded-md text-slate-700 bg-white"
           placeholder="Search..."
         />
-        <button className="btn bg-zinc-300 border-none hover:bg-slate-500">
+        <button type="submit" className="btn bg-zinc-300 border-none hover:bg-slate-500">
           <IoIosSearch size={22} className="text-slate-50" />
         </button>
       </form>
--- frontend/src/hooks/useGetMessages.jsx
+++ frontend/src/hooks/useGetMessages.jsx
@@ -10,9 +10,7 @@ function useGetMessages() {
     const fetchMessages = async()=>{
         try {
             const res = await axios.get(`http://localhost:8000/api/v1/message/${selectedUser?._id}`, {withCredentials:true})
-            dispatch(setMessage(res.data.messages))
-            console.log(res);
-            
+            dispatch(setMessage(res.data.messages))     
         } catch (error) {
            console.log(error); 
         }
