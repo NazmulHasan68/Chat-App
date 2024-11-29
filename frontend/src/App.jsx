fe74f452007d8a2f409fc5f7abaccad00978ddfa
NazmulHasan68 <aishadowzone@gmail.com>
2024-11-29 07:26:26 +0600

    online user setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,10 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Signup from '../src/components/Signup'
 import HomePage from "./components/HomePage"
 import Login from "./components/Login"
-import { useSelector } from "react-redux"
-import {io} from 'socket.io-client'
+import { useDispatch, useSelector } from "react-redux"
+import {io, Socket} from 'socket.io-client'
+import { setSocket } from "./redux/socketSlice"
+import { setOnlineUsers } from "./redux/userSlice"
 
 
 const router = createBrowserRouter([
@@ -23,18 +25,28 @@ const router = createBrowserRouter([
 ])
 
 export default function App() {
-  const [socket, setSocket] = useState(null);
   const { authUser } = useSelector((store) => store.user);
+  const { socket } = useSelector((store) => store.socket);
+  const dispatch = useDispatch()
+
   useEffect(() => {
     if (authUser) {
       const newSocket = io("http://localhost:8000", {
         query:{userId:authUser._id}
       });
-      setSocket(newSocket);
-      return () => {
-        newSocket.disconnect();
-        setSocket(null);
-      };
+      dispatch(setSocket(newSocket))
+     
+      newSocket.on('getOnlineUsers', (onlineUsers)=>{
+        dispatch(setOnlineUsers(onlineUsers))
+      })  
+
+      return ()=> socket.close()
+      
+    }else{
+      if(socket){
+        socket.close()
+        dispatch(setSocket(null))
+      }
     }
   }, [authUser]);
   return (
--- frontend/src/components/OtherUser.jsx
+++ frontend/src/components/OtherUser.jsx
@@ -4,15 +4,17 @@ import { setSelectedUser } from '../redux/userSlice'
 
 export default function OtherUser({user}) {
     const dispatch = useDispatch()
+    const {selectedUser, onlineUsers} = useSelector(store=>store.user)
+   
+    const isOnline = onlineUsers?.includes(user._id);
     const seletecUserhandler =(user)=>{
         dispatch(setSelectedUser(user))
     }
-    const {selectedUser} = useSelector(store=>store.user)
     
   return (
     <div>
         <div onClick={()=>seletecUserhandler(user)} className={`${selectedUser?._id === user._id? 'bg-white/50 ': '' } flex items-center gap-4 bg-slate-50/20 hover:bg-slate-50/60 rounded-md  cursor-pointer  p-2`}>
-            <div className='avatar online'>
+            <div className={`avatar ${isOnline? 'online': ''}`}>
                 <div className='w-10 h-10 rounded-full'>
                     <img alt='dp' src={user?.profilePhoto}/>
                 </div>
